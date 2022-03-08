bab228a435aa16dcbc29e21c44e587d54ac19607
Clara-Brandt <brand22c@mtholyoke.edu>
2022-03-08 13:56:00 -0500

    react and express combined

--- react-app/co-op/src/App.js
+++ react-app/co-op/src/App.js
@@ -1,8 +1,40 @@
+//demo code from https://www.freecodecamp.org/news/create-a-react-frontend-a-node-express-backend-and-connect-them-together-c5798926047c/
+
+import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
+//import dotenv from 'dotenv';
 
-function App() {
-  return (
+class App extends Component{
+  constructor(props){
+  super(props);
+  this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/test/API")
+      .then(res => res.text())
+      .then(res => this.setState({apiResponse: res}))
+      .catch(err => err);
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-Header">
+          <img src={logo} className="App-logo" alt ="logo"/>
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-title">{this.state.apiResponse}</p>
+      </div>
+    );
+  }
+
+  /*return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +52,7 @@ function App() {
       </header>
     </div>
   );
+  */
 }
 
 export default App;
