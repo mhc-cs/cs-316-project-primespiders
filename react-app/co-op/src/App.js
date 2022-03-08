12e0caed631ad23e4326faeab66be6d27a7e9d62
MHKlein1436 <81051806+MHKlein1436@users.noreply.github.com>
2022-03-08 14:33:42 -0500

    Remove slash

--- react-app/co-op/src/App.js
+++ react-app/co-op/src/App.js
@@ -12,7 +12,7 @@ class App extends Component{
   }
 
   callAPI() {
-    fetch("http://localhost:9000/test/API")
+    fetch("http://localhost:9000/testAPI")
       .then(res => res.text())
       .then(res => this.setState({apiResponse: res}))
       .catch(err => err);
