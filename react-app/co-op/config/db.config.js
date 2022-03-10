23414a7544658f0166260bef847d1afef79bac64
MHKlein1436 <81051806+MHKlein1436@users.noreply.github.com>
2022-03-10 02:19:51 -0500

    Connect DB

---
+++ react-app/co-op/config/db.config.js
@@ -0,0 +1,9 @@
+//https://www.bezkoder.com/react-node-express-mysql/
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "spider3",
+    DB: "mysql_coop",
+    dialect: "mysql"
+  };
+
