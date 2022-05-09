d7d8baaab751dab90198f925eba481fcd5bb4191
ergitlin <gitli22e@mtholyoke.edu>
2022-05-09 11:41:08 -0400

    Fixed baseServerURL for local use

--- Frontend/src/constants.js
+++ Frontend/src/constants.js
@@ -1,2 +1,6 @@
-//export const baseServerURL = "http://cs-vm-05.cs.mtholyoke.edu:31600"
-export const baseServerURL = "http://localhost:31600"
+
+//cs-vm-05 is used when running the website from the MHC vm
+// export const baseServerURL = "http://cs-vm-05.cs.mtholyoke.edu:31600"
+
+//localhost is used when running the server and the frontend locally
+export const baseServerURL = "http://localhost:31600" 
